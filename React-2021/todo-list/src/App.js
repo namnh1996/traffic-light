c5518dc35bb77fdb5d67adce353bec00f868ad45
HAINAM-7 <user@users-MacBook-Pro.local>
2021-06-03 23:12:56 +0700

    finished todo-list

--- React-2021/todo-list/src/App.js
+++ React-2021/todo-list/src/App.js
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem';
 import React, {Component} from 'react';
+import tick from './images/check.svg';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
+      newItem: '',
       todoItems: [
-        { title: 'Mua bim bim', isComplete: true},
-        { title: 'Đi đá bóng', isComplete: true},
-        { title: 'Đi đổ xăng'}
+        // { title: 'Mua bim bim', isComplete: true},
+        // { title: 'Đi đá bóng', isComplete: true},
+        // { title: 'Đọc sách'},
+        // { title: 'Đi ngủ'},
+        // { title: 'Dậy sớm để thành công'}
     ]};
+    this.onKeyUp= this.onKeyUp.bind(this);
+    this.onChange= this.onChange.bind(this);
   }
   onItemClicked(item) {
     //console.log('Item Clicked', item);
@@ -33,8 +39,46 @@ class App extends Component {
       }
   }
 
+  onKeyUp(event){
+    if(event.keyCode === 13){
+    let text = event.target.value;
+    if(!text || text === ''){
+      return;
+    }
+
+    //xoa cac dau cach
+    text = text.trim();
+    if(!text){return;}
+      this.setState({
+        newItem: '',
+        todoItems: [
+          {title: text, isComplete: false},
+          ...this.state.todoItems
+        ]
+      })
+    }
+  }
+
+  onChange(event){
+    this.setState({
+      newItem: event.target.value
+    })
+  }
+
   render(){
     return <div className="App">
+      <div className="header">
+        <img src={tick} width={32}></img>
+        <input 
+          type="text" 
+          placeholder="Add a new item" 
+          onKeyUp={this.onKeyUp}
+          value={this.state.newItem}
+          onChange={this.onChange}
+        >
+          
+        </input>
+      </div>
       {this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) =>
        (
         <TodoItem 
